619277432e53f7225fd04367e3aa1110fd8a06a4
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2022-12-20 12:05:40 +0100

    create Authentication Breeze update

--- Prototypes/Prototype-Basic/app/Http/Controllers/TodoController.php
+++ Prototypes/Prototype-Basic/app/Http/Controllers/TodoController.php
@@ -7,4 +7,15 @@ use Illuminate\Http\Request;
 class TodoController extends Controller
 {
     //
+    public function index (){
+        return Todo::all();
+    }
+
+    public function create(){
+        
+    }
+
+    public function store(Request $request){
+
+    }
 }
